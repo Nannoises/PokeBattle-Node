d05fb6f258dfe3f1a5c481963b9cbcef3e5790e8
Matt <birdmanherron@gmail.com>
2016-06-16 11:45:39 -0400

    Add route for image count.

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.get('/', function(request, response) {
 app.get('/image', function(request, response){
   response.sendfile('009.png');
 })
-
+app.get('/imagecount', function(request, response){
+  response.end(JSON.stringify(fs.readdirSync(__dirname + '/blastoise50')));
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
