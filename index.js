f6b87469122643b9db8446e8f82463bddc3bda4a
Matt <birdmanherron@gmail.com>
2016-06-16 11:23:47 -0400

    Include blastoise test files for static serving.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/blastoise50'))
 var fs = require('fs');
 console.log("fs loaded");
 // read binary data
