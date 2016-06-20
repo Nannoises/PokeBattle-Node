0e225f38535553e73424801bf23c03a4c07a3953
Matt <birdmanherron@gmail.com>
2016-06-20 15:37:40 -0400

    Fix typo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var responseData = {
 app.get('/', function(request, response) {
   var responseText = fs.readFileSync('settings.html', {'encoding': "utf8"});
   responseText = responseText.replace('ALLYNAMEKEY', request.param('AllyName'));
-  responseText = resposneText.replace('ENEMYNAMEKEY', request.param('EnemyName'))
+  responseText = responseText.replace('ENEMYNAMEKEY', request.param('EnemyName'))
   response.end(responseText);
   //response.sendfile('settings.html');
   //response.end(JSON.stringify(responseData));
