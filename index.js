8e500a3c18f445128deddb9b91f4e80a00f703af
Matt <birdmanherron@gmail.com>
2016-06-30 13:34:22 -0400

    Add route for settings-custom.html

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ app.get('/', function(request, response) {
   //response.sendfile('settings.html');
   //response.end(JSON.stringify(responseData));
 })
+app.get('/custom', function(request, response) {
+  var responseText = fs.readFileSync('settings-custom.html', {'encoding': "utf8"});
+  responseText = responseText.replace('ALLYNAMEKEY', request.param('AllyName') || '');
+  responseText = responseText.replace('ENEMYNAMEKEY', request.param('EnemyName') || '');
+  if(request.param('FocusAnimate') && (request.param('FocusAnimate') == 1 || request.param('FocusAnimate') == 'true' || request.param('FocusAnimate') == 'True'))
+    responseText = responseText.replace('FOCUSANIMATEKEY', 'checked="true"');
+  if(request.param('FlickAnimate') && (request.param('FlickAnimate') == 1 || request.param('FlickAnimate') == 'true' || request.param('FlickAnimate') == 'True'))
+    responseText = responseText.replace('FLICKANIMATEKEY', 'checked="true"');    
+  response.end(responseText);
+  //response.sendfile('settings.html');
+  //response.end(JSON.stringify(responseData));
+})
 app.get('/image', function(request, response){
   response.sendfile('009.png');
 })
