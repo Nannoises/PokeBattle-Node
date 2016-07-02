58c354797ecdfa63b13b7182a1ca787389fc162d
Matt <birdmanherron@gmail.com>
2016-07-02 17:25:50 -0400

    Remove flip.

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ util.inherits(TransformStream, Transform);
 TransformStream.prototype._transform = function(chunk, encoding, callback) {
   console.log('transform before : ' + JSON.stringify(chunk));
  
-  var flipped = gm(chunk, 'image.png').flip().toBuffer('PNG', function(err, buffer){
+  var flipped = gm(chunk, 'image.png').toBuffer('PNG', function(err, buffer){
     console.log('Inside toBuffer callback. err: ' + err);
     console.log('flipped: ' + JSON.stringify(flipped));
     console.log('transform after : ' + JSON.stringify(chunk));
