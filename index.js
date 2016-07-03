c34f04bc77ebda373056ca29e9891667b2a32fb1
Matt <birdmanherron@gmail.com>
2016-07-03 16:18:00 -0400

    Try out.

--- index.js
+++ index.js
@@ -47,12 +47,10 @@ app.get('/formatImage', function(request, response) {
   webRequest.get({url: 'https://s31.postimg.org/zetnmyy8b/Tyrantrumfor_DA_zpse9d7d288.png', encoding: null}, function(error, innerResponse, body){
     console.log('recieved body: ' + JSON.stringify(body));
     var command = gm(body)
-      .in('+dither -remap pebble_colors_64.gif');
+      .out('+dither -remap pebble_colors_64.gif');
     console.log('gm command: ' + JSON.stringify(command));
     command.stream(function(err, stdout, stderr){
-      if(err){
-        console.log('err: ' + err);
-      }
+      console.log('err: ' + err);
       stdout.pipe(response);
     });
   });
