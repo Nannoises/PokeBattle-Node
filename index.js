1e1c1f3c2fee0daf7844514988f612d0b399d83d
Matt <birdmanherron@gmail.com>
2016-07-02 21:03:30 -0400

    Map.

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/formatImage', function(request, response) {
   webRequest.get({url: 'https://s31.postimg.org/zetnmyy8b/Tyrantrumfor_DA_zpse9d7d288.png', encoding: null}, function(error, innerResponse, body){
     console.log('recieved body: ' + JSON.stringify(body));
     var command = gm(body)
-      .out('-remap pebble_colors_64.gif');
+      .out('-map pebble_colors_64.gif');
     console.log('gm command: ' + JSON.stringify(command));
     command.stream(function(err, stdout, stderr){
       stdout.pipe(response);
