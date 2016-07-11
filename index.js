30fe3a8ad4fd9e67e7a923ad0ec9063b1a8238f7
Matt <birdmanherron@gmail.com>
2016-07-11 13:19:07 -0400

    Just resize, comment out colors.

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ app.get('/formatImage', function(request, response) {
   webRequest.get({url: 'https://s31.postimg.org/zetnmyy8b/Tyrantrumfor_DA_zpse9d7d288.png', encoding: null}, function(error, innerResponse, body){
     console.log('recieved body: ' + JSON.stringify(body));
     var command = gm(body)
-      .resize(96,96)
-      .colors(64);
+      .resize(96,96);
+      //.colors(64);
       //.in('-map pebble_colors_64.gif');
       //.out('+dither -remap pebble_colors_64.gif');
     console.log('gm command: ' + JSON.stringify(command));
