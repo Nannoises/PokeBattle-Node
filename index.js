d1084396c75961a6daa2461c43537df6de6b1f6c
Matt <birdmanherron@gmail.com>
2016-07-15 14:45:07 -0400

    limit to 20?

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/pokemonNames', function(request, response){
   if(pokemonNames !== undefined){
      response.end(JSON.stringify(pokemonNames));
   } else {
-    webRequest('http://pokeapi.co/api/v2/pokemon?limit=1000', function (error, innerResponse, body) {
+    webRequest('http://pokeapi.co/api/v2/pokemon', function (error, innerResponse, body) {
       if (!error && response.statusCode == 200) {
         console.log('body ' + body);
         var results = body.results;
