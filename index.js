704b15193e806ee37238616edee05e66198015ae
Matt <birdmanherron@gmail.com>
2016-07-15 16:41:41 -0400

    Regex replace gifs.

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.get('/getSprites', function(request, response){
     console.log("InnerResponse: " + innerResponse);
     console.log("Body:" + body);
     //Remove gifs.
-
+    body = body.replace(/<img src=".*\.gif.*>/g, "");
     response.end(body);
   });
 });
